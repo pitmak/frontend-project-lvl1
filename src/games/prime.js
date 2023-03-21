b8f368d206efcf6c0b4c3ff4a94e540ce1ec15e3
pitmak <piter_makarov@mail.ru>
2023-03-21 22:18:08 +0500

    fix

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,5 +1,5 @@
 const isPrime = (num) => {
-  for (let i = 2, s = Math.sqrt(num); i <= s; i++) {
+  for (let i = 2, s = Math.sqrt(num); i <= s; i += 1) {
     if (num % i === 0) {
       return false;
     }
